96693c63b5657acffc2ea11fe48083a6db3fdeb1
sutedalm <37000551+sutedalm@users.noreply.github.com>
2021-11-13 22:39:10 +0100

    reafactor: add constant transportation types

--- utils/api-calls.js
+++ utils/api-calls.js
@@ -1,21 +1,28 @@
+import { TRANSPORTATION_TYPES } from "./constants";
+
 // Makes api call to get recipe and shop list
 export async function api_call(from, to) {
-    cards = [
-        {
-            "type": "flight",
-            "carbon_emission": 10,
-            "link": "https://www.google.com/",
-        },
-        {
-            "type": "train",
-            "carbon_emission": 10,
-            "link": "https://www.google.com/",
-        },
-        {
-            "type": "car",
-            "carbon_emission": 10,
-            "link": "https://www.google.com/",
-        },
-    ]
-    return card
+  cards = [
+    {
+      type: TRANSPORTATION_TYPES.FLIGHT,
+      carbon_emission: 10,
+      link: "https://www.google.com/",
+    },
+    {
+      type: TRANSPORTATION_TYPES.TRAIN,
+      carbon_emission: 10,
+      link: "https://www.google.com/",
+    },
+    {
+      type: TRANSPORTATION_TYPES.BUS,
+      carbon_emission: 10,
+      link: "https://www.google.com/",
+    },
+    {
+      type: TRANSPORTATION_TYPES.CAR,
+      carbon_emission: 10,
+      link: "https://www.google.com/",
+    },
+  ];
+  return cards;
 }
