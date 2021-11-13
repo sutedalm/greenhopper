55765f291b5ee32c216e12322001092fa8d1149c
Prakita Renatin <ge25zow@mytum.de>
2021-11-13 21:25:59 +0100

    Added utils folder

---
+++ utils/api-calls.js
@@ -0,0 +1,21 @@
+// Makes api call to get recipe and shop list
+export async function api_call(from, to) {
+    cards = [
+        {
+            "type": "flight",
+            "carbon_emission": 10,
+            "link": "https://www.google.com/",
+        },
+        {
+            "type": "train",
+            "carbon_emission": 10,
+            "link": "https://www.google.com/",
+        },
+        {
+            "type": "car",
+            "carbon_emission": 10,
+            "link": "https://www.google.com/",
+        },
+    ]
+    return card
+}
