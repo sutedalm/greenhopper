5781cea0dc713c7ead268a586bf29c8611f5d679
sutedalm <37000551+sutedalm@users.noreply.github.com>
2021-11-14 16:28:29 +0100

    change travelcardlist to flexbox

--- components/travelItems/TravelCardList/TravelCardList.js
+++ components/travelItems/TravelCardList/TravelCardList.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Swiper, SwiperSlide } from "swiper/react";
 import styles from "./TravelCardList.module.css";
 
 import CardItem from "../CardItem/CardItem";
@@ -47,7 +46,7 @@ const TravelCardList = ({ from, to, startDate, endDate, co2_cards }) => {
           FLIGHTS
         </h1>
         <hr className={styles.travelCardListContainer__headingLine} />
-        <Swiper slidesPerView={"auto"} spaceBetween={3} direction="vertical">
+        <div className={styles.travelCardListContainer__listContainer}>
           {cards.map(
             ({
               type,
@@ -58,7 +57,7 @@ const TravelCardList = ({ from, to, startDate, endDate, co2_cards }) => {
               endDate,
               duration,
             }) => (
-              <SwiperSlide key={type} className={styles.swiperSlide}>
+              <div key={type} className={styles.swiperSlide}>
                 <CardItem
                   type={type}
                   carbon_emission={carbon_emission}
@@ -70,10 +69,10 @@ const TravelCardList = ({ from, to, startDate, endDate, co2_cards }) => {
                   price={price}
                   duration={duration}
                 />
-              </SwiperSlide>
+              </div>
             )
           )}
-        </Swiper>
+        </div>
       </div>
     </div>
   );
